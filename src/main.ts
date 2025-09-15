d2aa64ff5b3d082e33e318093271a3f17b545b70
Luís Felipe Simões <luis.felipe-simoes@outlook.com>
2025-09-15 22:52:27 +0000

    feat: update NestJS configuration and add validation with Swagger integration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = parseInt(process.env['API_PORT'] || '3000', 10);
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<string>('API_PORT', process.env.PORT) || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Portal ONG Ser Amor API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+
   await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
