919b420e44c83b07dd22efcdae0300d9618dfaeb
Luís Felipe Simões <luis.felipe-simoes@outlook.com>
2025-09-16 01:52:57 +0000

    feat: integrate TypeORM with PostgreSQL and add database configuration
    - Added @nestjs/typeorm and pg dependencies to package.json.
    - Configured TypeOrmModule in app.module.ts to connect to PostgreSQL using environment variables for database configuration.
    - Set up entity auto-loading and migration settings.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,45 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>(
+          'DATABASE_HOST',
+          process.env.DATABASE_HOST,
+        ),
+        port: parseInt(
+          configService.get<string>('DATABASE_PORT', process.env.DATABASE_PORT),
+          10,
+        ),
+        username: configService.get<string>(
+          'DATABASE_USER',
+          process.env.DATABASE_USER,
+        ),
+        password: configService.get<string>(
+          'DATABASE_PASSWORD',
+          process.env.DATABASE_PASSWORD,
+        ),
+        database: configService.get<string>(
+          'DATABASE_NAME',
+          process.env.DATABASE_NAME,
+        ),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: false,
+        migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+        migrationsRun: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
