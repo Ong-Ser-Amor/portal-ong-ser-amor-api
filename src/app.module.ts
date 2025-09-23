bc30b063ad5d8085c9b32e9455a38063d910cf14
Luís Felipe Simões <luis.felipe-simoes@outlook.com>
2025-09-23 04:04:54 +0000

    fix: correct import order in AppController and AppModule for consistency

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import { StudentsModule } from './students/students.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
