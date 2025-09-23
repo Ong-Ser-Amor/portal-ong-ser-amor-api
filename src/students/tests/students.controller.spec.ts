b0220d5a5fecf36e4fcc956fb735c8072c8f9763
Luís Felipe Simões <luis.felipe-simoes@outlook.com>
2025-09-23 04:27:55 +0000

    refactor: remove unused import for InternalServerErrorException in students.controller.spec.ts

--- src/students/tests/students.controller.spec.ts
+++ src/students/tests/students.controller.spec.ts
@@ -1,7 +1,4 @@
-import {
-  InternalServerErrorException,
-  NotFoundException,
-} from '@nestjs/common';
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { StudentResponseDto } from '../dto/student-response.dto';
