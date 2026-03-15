0a194ddad338eee56a3c4687753d51fd777d0c92
luisxfelipe <luis.felipe-simoes@outlook.com>
2026-03-15 21:43:28 +0000

    feat: implementar CreateContactDto e  ContactResponseDto

--- src/contacts/dto/create-contact.dto.ts
+++ src/contacts/dto/create-contact.dto.ts
@@ -1,1 +1,22 @@
-export class CreateContactDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+import { ContactType } from '../enums/contact-type.enum';
+
+export class CreateContactDto {
+  @ApiProperty({
+    enum: ContactType,
+    example: ContactType.MOBILE,
+  })
+  @IsEnum(ContactType)
+  @IsNotEmpty()
+  contactType: ContactType;
+
+  @ApiProperty({
+    example: '1234567890',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  value: string;
+}
