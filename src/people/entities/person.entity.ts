676d0304dbb55e8828d83fcb43d996e302e40fba
luisxfelipe <luis.felipe-simoes@outlook.com>
2026-03-25 02:38:22 +0000

    feat: adicionar a relação de tutor e os campos à entidade Pessoa e à migração.

--- src/people/entities/person.entity.ts
+++ src/people/entities/person.entity.ts
@@ -3,6 +3,8 @@ import {
   CreateDateColumn,
   DeleteDateColumn,
   Entity,
+  JoinColumn,
+  ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
@@ -29,6 +31,23 @@ export class Person {
 
   @Column({ name: 'birth_date', type: 'date', nullable: false })
   birthDate: Date;
+
+  @Column({ name: 'can_leave_alone', type: 'boolean', nullable: true })
+  canLeaveAlone: boolean;
+
+  @Column({ name: 'guardian_id', type: 'bigint', nullable: true })
+  guardianId: string;
+
+  // O "Lado Dono" do relacionamento (Quem tem a chave estrangeira)
+  @ManyToOne(() => Person, (person) => person.dependents, {
+    onDelete: 'SET NULL',
+  })
+  @JoinColumn({ name: 'guardian_id' })
+  guardian: Person;
+
+  // O "Lado Inverso" (Para poder buscar uma pessoa e ver todos os dependentes dela de uma vez)
+  @OneToMany(() => Person, (person) => person.guardian)
+  dependents: Person[];
 
   @OneToMany(() => PersonContact, (personContact) => personContact.person)
   personContacts: PersonContact[];
