d5a818beb544f9c093e12313ac0ffb8bfb476ef2
luisxfelipe <luis.felipe-simoes@outlook.com>
2026-03-25 02:33:15 +0000

    feat: implementar CreateFamilyDto e FamilyResponseDto para representar os dados da família.

--- src/families/dto/create-family.dto.ts
+++ src/families/dto/create-family.dto.ts
@@ -1,1 +1,44 @@
-export class CreateFamilyDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
+import { HousingType } from '../enums/housing-type.enum';
+import { IncomeRange } from '../enums/income-range.enum';
+
+export class CreateFamilyDto {
+  @ApiProperty({
+    enum: IncomeRange,
+    description: 'The income range of the family',
+    example: IncomeRange.FROM_1_TO_3_MINIMUM_WAGE,
+  })
+  @IsEnum(IncomeRange, {
+    message: `incomeRange must be a valid enum value of IncomeRange`,
+  })
+  @IsNotEmpty()
+  incomeRange: IncomeRange;
+
+  @ApiProperty({
+    description: 'Indicates whether the family receives income transfer',
+    example: true,
+  })
+  @IsBoolean({ message: 'receivesIncomeTransfer must be a boolean value' })
+  receivesIncomeTransfer: boolean;
+
+  @ApiProperty({
+    enum: HousingType,
+    description: 'The type of housing the family has',
+    example: HousingType.RENTED,
+  })
+  @IsEnum(HousingType, {
+    message: `housingType must be a valid enum value of HousingType`,
+  })
+  @IsNotEmpty()
+  housingType: HousingType;
+
+  @ApiProperty({
+    description: 'The ID of the address associated with the family',
+    example: '1',
+  })
+  @IsString({ message: 'addressId must be a string' })
+  @IsNotEmpty()
+  addressId: string;
+}
