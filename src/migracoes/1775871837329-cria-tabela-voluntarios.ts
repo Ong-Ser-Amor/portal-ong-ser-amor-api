dc442e7d67e48f3f27b005a42046d87e34568d4d
luisxfelipe <luis.felipe-simoes@outlook.com>
2026-04-11 01:58:12 +0000

    feat: cria migração para a tabela de voluntários

---
+++ src/migracoes/1775871837329-cria-tabela-voluntarios.ts
@@ -0,0 +1,84 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CriaTabelaVoluntarios1775871837329 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'voluntarios',
+        columns: [
+          {
+            name: 'id',
+            type: 'bigint',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'identity',
+          },
+          {
+            name: 'pessoa_id',
+            type: 'bigint',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'formacao_academica',
+            type: 'varchar',
+            length: '100',
+            isNullable: true,
+          },
+          {
+            name: 'status_formacao',
+            type: 'varchar',
+            length: '100',
+            isNullable: true,
+          },
+          {
+            name: 'tipo_voluntariado',
+            type: 'varchar',
+            length: '100',
+            isNullable: false,
+          },
+          {
+            name: 'criado_em',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'atualizado_em',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'deletado_em',
+            type: 'timestamp',
+            isNullable: true,
+          },
+        ],
+      }),
+      true,
+    );
+
+    await queryRunner.createForeignKey(
+      'voluntarios',
+      new TableForeignKey({
+        name: 'fk_voluntarios_pessoa_id',
+        columnNames: ['pessoa_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'pessoas',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('voluntarios', 'fk_voluntarios_pessoa_id');
+    await queryRunner.dropTable('voluntarios');
+  }
+}
