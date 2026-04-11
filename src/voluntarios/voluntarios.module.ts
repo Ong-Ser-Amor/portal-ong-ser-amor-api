9ab08f36a4fa52817ec4bc5c65ea5c32b519bfb6
luisxfelipe <luis.felipe-simoes@outlook.com>
2026-04-11 03:25:48 +0000

    feat: adiciona módulo de voluntários

--- src/voluntarios/voluntarios.module.ts
+++ src/voluntarios/voluntarios.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { VoluntariosService } from './voluntarios.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Voluntario } from './entities/voluntario.entity';
 import { VoluntariosController } from './voluntarios.controller';
+import { VoluntariosService } from './voluntarios.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Voluntario])],
   controllers: [VoluntariosController],
   providers: [VoluntariosService],
+  exports: [VoluntariosService],
 })
 export class VoluntariosModule {}
