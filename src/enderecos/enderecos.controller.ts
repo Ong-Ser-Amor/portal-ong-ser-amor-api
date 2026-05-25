9f48ba2fa3f8d326b2a1fca69df95813270b5dc1
luisxfelipe <luis.felipe-simoes@outlook.com>
2026-05-25 23:33:18 +0000

    feat: adiciona controlador e DTO para atualização de endereços, implementa operação de atualização

---
+++ src/enderecos/enderecos.controller.ts
@@ -0,0 +1,41 @@
+import { Body, Controller, Param, Patch } from '@nestjs/common';
+import {
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+
+import { AtualizarEnderecoDto } from './dto/atualizar-endereco.dto';
+import { EnderecoRespostaDto } from './dto/endereco-resposta.dto';
+import { EnderecosService } from './enderecos.service';
+
+@ApiTags('Endereços')
+@Controller('enderecos')
+export class EnderecosController {
+  constructor(private readonly enderecosService: EnderecosService) {}
+
+  @Patch(':id')
+  @ApiOperation({ summary: 'Atualizar os dados de um endereço' })
+  @ApiOkResponse({
+    description: 'O endereço foi atualizado com sucesso.',
+    type: EnderecoRespostaDto,
+  })
+  @ApiNotFoundResponse({
+    description: 'Endereço com o ID especificado não encontrado.',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Ocorreu um erro inesperado ao atualizar o endereço.',
+  })
+  async atualizar(
+    @Param('id') id: string,
+    @Body() atualizarEnderecoDto: AtualizarEnderecoDto,
+  ): Promise<EnderecoRespostaDto> {
+    const enderecoAtualizado = await this.enderecosService.atualizar(
+      id,
+      atualizarEnderecoDto,
+    );
+    return new EnderecoRespostaDto(enderecoAtualizado);
+  }
+}
--- src/enderecos/enderecos.module.ts
+++ src/enderecos/enderecos.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { EnderecosService } from './enderecos.service';
 import { Endereco } from './entities/endereco.entity';
+import { EnderecosController } from './enderecos.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Endereco])],
   providers: [EnderecosService],
   exports: [EnderecosService],
+  controllers: [EnderecosController],
 })
 export class EnderecosModule {}
