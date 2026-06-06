952b0a3f9749a4fc3084cd5ff8996f93a14a9a32
luisxfelipe <luis.felipe-simoes@outlook.com>
2026-06-06 14:28:33 +0000

    feat: adiciona migração para criar a tabela de cursos

---
+++ src/migracoes/1780755592111-cria-tabela-cursos.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CriaTabelaCursos1780755592111 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'cursos',
+        columns: [
+          {
+            name: 'id',
+            type: 'bigint',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'identity',
+          },
+          {
+            name: 'nome',
+            type: 'varchar',
+            length: '100',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'descricao',
+            type: 'text',
+            isNullable: true,
+          },
+          {
+            name: 'criado_em',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'atualizado_em',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'deletado_em',
+            type: 'timestamp',
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('cursos');
+  }
+}
