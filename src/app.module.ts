1fdffb22f1fdd6bea815ae0b01220d0f63229dee
luisxfelipe <luis.felipe-simoes@outlook.com>
2026-07-13 23:33:23 +0000

    refatoração: remove o módulo Courses e o código relacionado

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,6 @@ import { AutenticacaoModule } from './autenticacao/autenticacao.module';
 import { BeneficiariosModule } from './beneficiarios/beneficiarios.module';
 import { ChamadasModule } from './chamadas/chamadas.module';
 import { ContatosModule } from './contatos/contatos.module';
-import { CoursesModule } from './courses/courses.module';
 import { CursosModule } from './cursos/cursos.module';
 import { EnderecosModule } from './enderecos/enderecos.module';
 import { FamiliasModule } from './familias/familias.module';
@@ -58,7 +57,6 @@ import { VoluntariosModule } from './voluntarios/voluntarios.module';
     AssetCategoriesModule,
     BeneficiariosModule,
     ContatosModule,
-    CoursesModule,
     EnderecosModule,
     FamiliasModule,
     LocationsModule,
--- src/courses/dto/create-course.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class CreateCourseDto {
-  @ApiProperty({ type: String, example: 'Carlos Santos' })
-  @IsNotEmpty({ message: 'Name is required' })
-  @IsString({ message: 'Name must be a string' })
-  name: string;
-}
